529a442a97efc73b6cbba707cc8930d90617e83f
Kiranug <kiran.ug123@gmail.com>
2021-08-06 18:22:55 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -34,6 +34,7 @@ resource "google_container_node_pool" "primary_nodes" {
   location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
+  enable_private_nodes = true
 
   node_config {
     oauth_scopes = [
